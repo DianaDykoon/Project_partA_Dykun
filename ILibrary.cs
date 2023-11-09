d5826a141ff61b3377bb0ce4104865d7aa901b71
Diana Dykun <diana.dykoon@gmail.com>
2023-11-09 12:40:57 +0200

    Добавила интерфейсы 'IPerson', 'ILibrary'

---
+++ ILibrary.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Project_partA_Dykun
+{
+    public interface ILibrary
+    {
+        string Address { get; set; }
+
+        public void AddBook(string title, Author author, int numberOfPages, Genre genre, int yearOfWriting, Publisher publisher);
+
+        public Book GetBook(string title);
+
+        public void RemoveBook(string title);
+
+        public List<Book> GetCatalog();
+
+        public string ToString();
+
+    }
+}
---
+++ IPerson.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Project_partA_Dykun
+{
+    public interface IPerson
+    {
+        string FirstName { get; set; }
+
+        string LastName { get; set; }
+
+        int Age { get; set; }
+
+        public string FullName
+        {
+            get;
+        }
+
+        public string ToString();
+    }
+}
