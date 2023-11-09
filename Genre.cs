c5c20018630a9111317b76dface1077885ab966f
Diana Dykun <diana.dykoon@gmail.com>
2023-11-09 12:43:22 +0200

    Добавила enum 'Genre', 'Publisher'

---
+++ Genre.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Project_partA_Dykun
+{
+    public enum Genre
+    {
+        Novella,
+        History,
+        Detective,
+        Tragedy,
+        Comdey,
+        Mystery,
+        Thriller,
+        Business
+    }
+}
---
+++ Publisher.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Project_partA_Dykun
+{
+    public enum Publisher
+    {
+        HarperCollins,
+        SimonAndShuster,
+        Macmillan,
+        Scholastic,
+        Pearson
+    }
+}
