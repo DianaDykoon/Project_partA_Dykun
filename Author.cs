140cc86b80a7f30e52cf57f1d14af8371d70467e
Diana Dykun <diana.dykoon@gmail.com>
2023-11-09 12:46:18 +0200

    Добавила классы 'Author', 'Librarian

---
+++ Author.cs
@@ -0,0 +1,60 @@
+﻿
+
+namespace Project_partA_Dykun
+{
+    public class Author : IPerson
+    {
+        private string? _firstName;
+        private string? _lastName;
+        private int _age;
+        private string? _pseudonym;
+        private string? _nationality;
+        private int _fee;
+
+        public string FirstName
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public string LastName
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public int Age
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public string Pseudonym
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public string Nationality
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public int Fee
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public string FullName => throw new NotImplementedException();
+
+        public Author()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Librarian.cs
@@ -0,0 +1,53 @@
+﻿
+
+namespace Project_partA_Dykun
+{
+    public class Librarian : IPerson
+    {
+        private string? _firstName;
+        private string? _lastName;
+        private int _age;
+        private long? _phoneNumber;
+        private int _salary;
+
+        public string FirstName
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public string LastName
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public int Age
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public long PhoneNumber
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public int Salary
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public string FullName => throw new NotImplementedException();
+
+        public Librarian()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
